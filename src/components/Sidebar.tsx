1127384668b770b4651402d417b679721a31d84d
Felipe Parra <fpr29.10@gmail.com>
2024-02-25 12:36:15 -0600

    Fixed sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,26 +7,42 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { TbBaselineDensityLarge } from "react-icons/tb";
-import { ReactNode } from "react"
 import ButtonTheme from "./ButtonTheme";
+import { NavLinks } from "@/constants/nav-links";
+import { Link } from "react-router-dom";
+import { useUserContext } from "@/context/user/useUserContext";
+import { Button } from "./ui/button";
 
-export default function Sidebar({ children }: { children?: ReactNode }) {
+export default function Sidebar() {
+  const { user } = useUserContext()
   return (
     <Sheet>
       <SheetTrigger>
         <TbBaselineDensityLarge />
       </SheetTrigger>
-      <SheetContent side={"left"} className="bg-slate-900 text-slate-50">
-        {
-          children ??
-          <SheetHeader>
-            <SheetTitle className="text-secondary">Are you absolutely sure?</SheetTitle>
-            <SheetDescription className="text-secondary">
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
-        }
+      <SheetContent side={"left"} className="bg-primary text-slate-50 dark:bg-slate-900">
+        <SheetHeader>
+          <SheetTitle className="text-white dark:text-primary">Bank</SheetTitle>
+          <SheetDescription className="text-slate-50 dark:text-primary/70 text-justify my-2">
+            Best app to handle your accounts
+          </SheetDescription>
+          <article>
+            <section className="">
+              {NavLinks.map(({ id, to, name }, index) => {
+                if (user?.name !== undefined && id === "login") return null
+                return (
+                  <Button key={"nav-link-" + id + index} variant={"link"} className="w-full max-w-sm text-slate-50 dark:text-primary/70" asChild>
+                    <Link to={to}>
+                      {name}
+                    </Link>
+                  </Button>
+                )
+              })}
+            </section>
+          </article>
+
+        </SheetHeader>
+
         <article className="h-full w-full flex justify-between">
           <ButtonTheme />
         </article>
